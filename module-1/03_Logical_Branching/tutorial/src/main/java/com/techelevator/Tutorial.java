e520f37032f98d46f56b6ef91f8f811d0d2d039a
Katherine Schuljak <kschuljak@gmail.com>
2022-10-25 16:42:28 -0700

    Logical branching tutorial completed

--- module-1/03_Logical_Branching/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/03_Logical_Branching/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -15,27 +15,50 @@ public class Tutorial {
                 System.out.println("**************************************");
 
                 // Step Two: Declare and initialize Weather Station variables
-
+                int highTemperatureF = 55;
+                boolean precipitationExpected = true;
+                final int FREEZING_TEMPERATURE_F = 32;
 
 
                 // Step Three: Report today's expected conditions to the user.
-
+                System.out.println("Expected high temperature: " + highTemperatureF + "\u00B0F.");
+                if (precipitationExpected) {
+                        System.out.println("The chance of precipitation is high.");
+                } else {
+                        System.out.println("The chance of precipitation is low.");
+                }
 
 
                 // Step Four: Determine if the user should bring a coat. (when the temperature < 65°F)
-
+                /*
+                if (highTemperatureF < 65) {
+                        System.out.println("* Don't forget to bring a coat today.");
+                }
+                */
 
 
                 // Step Five: Determine if the user should bring an umbrella
-
+                boolean isAboveFreezing = highTemperatureF > FREEZING_TEMPERATURE_F;
+                if (precipitationExpected && isAboveFreezing) {
+                        System.out.println("* Be sure to pack an umbrella!");
+                }
 
 
                 // Step Six: Advise the user on the type of jacket they should be wearing. (<= 0°F, <= freezing, < 65°F)
-
+                if (highTemperatureF <= 0) {
+                        System.out.println("* Bundle up! Wear everything you have!");
+                } else if (!isAboveFreezing) {
+                        System.out.println("* Please wear a warm winter coat!");
+                } else if (highTemperatureF < 65) {
+                        System.out.println("* Take along a light jacket.");
+                }
 
 
                 // Step Seven: If the temperature delta for the day is more than 20°F, advise the user to dress in layers.
-
+                int lowTemperatureF = 34;
+                if (highTemperatureF - lowTemperatureF > 20) {
+                        System.out.println("* Dress in layers today, as the temperature may swing dramatically.");
+                }
                 
         }
 }
