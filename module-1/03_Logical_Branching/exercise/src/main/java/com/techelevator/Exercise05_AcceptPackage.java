90b1ba17d48e63fc220076f259588c6a66f44b6a
Katherine Schuljak <kschuljak@gmail.com>
2022-10-26 13:58:42 -0700

    03_Logical_Branching HW Exercise 5

--- module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise05_AcceptPackage.java
+++ module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise05_AcceptPackage.java
@@ -24,7 +24,7 @@ public class Exercise05_AcceptPackage {
     acceptPackage(50) ➔ false
      */
     public boolean acceptPackage(int weightPounds) {
-        return false;
+        return (weightPounds <= MAX_WEIGHT_POUNDS);
     }
 
     /*
@@ -41,7 +41,12 @@ public class Exercise05_AcceptPackage {
     acceptPackage(50, 4, 5, 10) ➔ false
      */
     public boolean acceptPackage(int weightPounds, int lengthInches, int widthInches, int heightInches) {
-        return false;
+        if (weightPounds > MAX_WEIGHT_POUNDS) {
+            return false;
+        } else {
+            double cubicInches = lengthInches * widthInches * heightInches;
+            return (cubicInches <= MAX_CUBIC_INCHES) ? true : false;
+        }
     }
 
     /*
@@ -64,6 +69,13 @@ public class Exercise05_AcceptPackage {
     acceptPackage(50, 4, 5, 10, true) ➔ false
      */
     public boolean acceptPackage(int weightPounds, int lengthInches, int widthInches, int heightInches, boolean isSurchargePaid) {
-        return false;
+        double cubicInches = lengthInches * widthInches * heightInches;
+        if (weightPounds > MAX_WEIGHT_POUNDS || cubicInches > MAX_CUBIC_INCHES) {
+            return false;
+        } else if ((lengthInches > MAX_DIMENSION_INCHES) || (widthInches > MAX_DIMENSION_INCHES) || (heightInches > MAX_DIMENSION_INCHES)) {
+            return (isSurchargePaid);
+        } else {
+            return true;
+        }
     }
 }
