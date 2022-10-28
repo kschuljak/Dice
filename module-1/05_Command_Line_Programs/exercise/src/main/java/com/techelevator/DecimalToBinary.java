9fc52c4dfb5a5c66aba331f90dc6be9fc151f1d9
Katherine Schuljak <kschuljak@gmail.com>
2022-10-28 13:21:12 -0700

    05_Command_Line exercises DecimalToBinary & Fibonacci finished

--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,26 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+
+		Scanner scannerInput = new Scanner(System.in);
+
+		System.out.print("Convert Decimals to Binary");
+		System.out.println();
+		System.out.print("Please enter a series of decimal values, separated by spaces: ");
+
+		String inputString = scannerInput.nextLine();
+		String[] inputList = inputString.split(" ");
+
+		for (int i = 0; i < inputList.length; i++) {
+
+			int decimal = Integer.parseInt(inputList[i]);
+			String binary =Integer.toBinaryString(decimal);
+			System.out.println(inputList[i] + " in binary is " + binary);
+		}
 
 	}
 
--- module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,45 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
 	public static void main(String[] args) {
+
+		Scanner inputScanner = new Scanner(System.in);
+
+		System.out.println("Print the Fibonacci Sequence (ending at entered number)");
+		System.out.println("-------------------------------------------------------");
+
+		System.out.print("Please enter a number: ");
+
+		String enteredNumberAsString = inputScanner.nextLine();
+		int enteredNumberAsInt = Integer.parseInt(enteredNumberAsString);
+
+
+		if (enteredNumberAsInt <= 0) System.out.print("0, 1");
+		else if (enteredNumberAsInt == 1) System.out.print("0, 1, 1");
+		else {
+			System.out.print("0, 1, 1, ");
+			int firstNumber = 1;
+			int secondNumber = 1;
+
+			while (secondNumber <= enteredNumberAsInt) {
+
+				int nextNumber = firstNumber + secondNumber;
+
+				if (nextNumber <= enteredNumberAsInt) {
+
+					System.out.print(nextNumber);
+					int nextNextNumber = secondNumber + nextNumber;
+					if (nextNextNumber <= enteredNumberAsInt) {
+						System.out.print(", ");
+					}
+				}
+				firstNumber = secondNumber;
+				secondNumber = nextNumber;
+			}
+		}
 
 	}
 
