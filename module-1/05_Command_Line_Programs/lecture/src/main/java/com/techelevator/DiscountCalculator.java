c44c23a51a95306a415ac65d9386a2867eaa1bf9
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-10-28 04:15:54 -0600

    added lecture code

---
+++ module-1/05_Command_Line_Programs/lecture/src/main/java/com/techelevator/DiscountCalculator.java
@@ -0,0 +1,32 @@
+package com.techelevator;
+
+import java.util.Scanner;
+
+class DiscountCalculator {
+
+    /**
+     * The main method is the start and end of our program
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Discount Calculator");
+
+        // Prompt the user for a discount amount
+        // The answer needs to be saved as a double
+        System.out.print("Enter the discount amount (w/out percentage): ");
+
+
+
+        // Prompt the user for a series of prices
+        System.out.print("Please provide a series of prices (space separated): ");
+
+
+
+
+
+
+    }
+
+}
