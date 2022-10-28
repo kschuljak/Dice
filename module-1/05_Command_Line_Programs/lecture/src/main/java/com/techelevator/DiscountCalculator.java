8a0739977e929dac253a1a8b2639d9e9c4df1f78
Katherine Schuljak <kschuljak@gmail.com>
2022-10-28 11:43:20 -0700

    DiscountCalculator.java coded

--- module-1/05_Command_Line_Programs/lecture/src/main/java/com/techelevator/DiscountCalculator.java
+++ module-1/05_Command_Line_Programs/lecture/src/main/java/com/techelevator/DiscountCalculator.java
@@ -17,16 +17,50 @@ class DiscountCalculator {
         // The answer needs to be saved as a double
         System.out.print("Enter the discount amount (w/out percentage): ");
 
+        String stringDiscount = scanner.nextLine();
+        double doubleDiscount = Double.parseDouble(stringDiscount);
 
 
         // Prompt the user for a series of prices
         System.out.print("Please provide a series of prices (space separated): ");
+        String priceInputLine = scanner.nextLine();
+        String[] priceValues = priceInputLine.split(" ");
+
+        double discountPercentage = doubleDiscount / 100;
+        System.out.println("Discount: " + doubleDiscount + "%");
 
 
+        for (int i = 0; i < priceValues.length; i++) {
+            double doublePrice = Double.parseDouble(priceValues[i]);
+            double discountTotal = doublePrice * discountPercentage;
+            double discountedPrice = doublePrice - discountTotal;
+            System.out.println("Original Price: $" + doublePrice);
+            System.out.println("Discounted Price: $" + discountedPrice);
+            System.out.println("You saved $" + discountTotal);
 
-
-
-
+        }
     }
+    /*
+    public static double discountConversion(double[] priceValues, double discount) {
 
+        double discountPercentage = discount / 100;
+        System.out.println("Discount: " + discount + "%");
+
+        for (int i = 0; i < priceValues.length; i++) {
+
+            double discountTotal = price * discountPercentage;
+            double discountedPrice = price - discountTotal;
+            System.out.println("Original Price: $" + price);
+            System.out.println("Discounted Price: $" + discountedPrice);
+            System.out.println("You saved $" + discountTotal);
+            return discountedPrice;
+        }
+    }
+    */
 }
+
+// create variables to store user input
+// parse Strings into doubles
+// pass in variables to conversion function
+// return result in function
+// output result
