0f99072d5e8e86a1c90598e5f1fa33fab0c62231
Katherine Schuljak <kschuljak@gmail.com>
2022-10-31 16:39:15 -0700

    08_Collections_Part2 tutorial finished

--- module-1/08_Collections_Part_2_Maps/tutorial/src/main/java/com/techelevator/Tutorial.java
+++ module-1/08_Collections_Part_2_Maps/tutorial/src/main/java/com/techelevator/Tutorial.java
@@ -10,11 +10,24 @@ public class Tutorial {
 
         // Step One: Declare a Map
 
+        Map<String, String> projects = new HashMap<String, String>();
     	
     	// Step Two: Add items to a Map
 
+        projects.put("Ada", "Inventing Computer Programming");
+        projects.put("Grace", "COBOL");
+        projects.put("Margaret", "Apollo Guidance Computer");
+        projects.put("Adele", "Graphical User Interfaces");
+
+        projects.remove("Grace");
     	
     	// Step Three: Loop through a Map
+
+        // for-each loop
+        // note: Map.Entry must have the same types as the Map being looped through
+        for (Map.Entry<String, String> project : projects.entrySet()) {
+            System.out.println("The key " + project.getKey() + " is linked to the value " + project.getValue());
+        }
 
     }
 
