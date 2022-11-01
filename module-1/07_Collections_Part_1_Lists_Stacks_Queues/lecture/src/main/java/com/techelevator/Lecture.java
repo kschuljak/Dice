b1c4ff325ee4f2980dfa4ca23c7d7f2c90ccfde9
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-11-01 05:32:51 -0600

    added lecture code lists

---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,67 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+		System.out.println("####################");
+		System.out.println("       LISTS");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists are ordered");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow duplicates");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow elements to be inserted in the middle");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists allow elements to be removed by index");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Find out if something is already in the List");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Find index of item in List");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be turned into an array");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be sorted");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("Lists can be reversed too");
+		System.out.println("####################");
+
+
+		System.out.println("####################");
+		System.out.println("       FOREACH");
+		System.out.println("####################");
+		System.out.println();
+
+
+	}
+}
