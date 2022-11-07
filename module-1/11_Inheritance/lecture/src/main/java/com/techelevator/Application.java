1a2d6a85aed862e44fc41d66fca6fb530f062b02
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-11-07 10:37:36 -0700

    adde lecture code for inheritance

---
+++ module-1/11_Inheritance/lecture/src/main/java/com/techelevator/Application.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // Create a new general auction
+        System.out.println("Starting a general auction");
+        System.out.println("-----------------");
+
+        Auction generalAuction = new Auction("Tech Elevator t-shirt");
+
+        generalAuction.placeBid(new Bid("Josh", 1));
+        generalAuction.placeBid(new Bid("Fonz", 23));
+        generalAuction.placeBid(new Bid("Rick Astley", 13));
+        //....
+        //....
+        // This might go on until the auction runs out of time or hits a max # of bids
+
+    }
+}
