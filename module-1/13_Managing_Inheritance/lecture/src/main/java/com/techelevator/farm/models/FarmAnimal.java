55afd7364c49e6c2015dc24bccb46c1844a7d2f5
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-11-09 09:07:48 -0700

    Added lecture code

---
+++ module-1/13_Managing_Inheritance/lecture/src/main/java/com/techelevator/farm/models/FarmAnimal.java
@@ -0,0 +1,24 @@
+package com.techelevator.farm.models;
+
+public class FarmAnimal implements Singable
+{
+    private String name;
+    private String sound;
+
+    public FarmAnimal(String name, String sound)
+    {
+        this.name = name;
+        this.sound = sound;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSound()
+    {
+        return sound;
+    }
+
+}
