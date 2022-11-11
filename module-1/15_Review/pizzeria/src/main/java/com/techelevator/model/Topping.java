d128b56c0b202c6883384afa066a15416de8e481
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-11-11 04:13:04 -0700

    added review code

---
+++ module-1/15_Review/pizzeria/src/main/java/com/techelevator/model/Topping.java
@@ -0,0 +1,24 @@
+package com.techelevator.model;
+
+import java.math.BigDecimal;
+
+public abstract class Topping {
+
+    private String name;
+    private BigDecimal price;
+
+    public Topping(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public abstract void eat();
+}
