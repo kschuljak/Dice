6d48f94bb78b34b8b06a1d5ce1fb5ab0abb7b873
Katherine Schuljak <kschuljak@gmail.com>
2022-11-15 09:02:10 -0800

    PersonalProject - added shop class to backpack package

---
+++ TestProject/DnDCreator/src/main/java/Backpack/Shop/Shop.java
@@ -0,0 +1,13 @@
+package Backpack.Shop;
+
+import Backpack.Coinpurse.*;
+import Backpack.Gear.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+
+    protected int shopFundsInCopper = 150000;
+    protected List<Gear> gearInShop = new ArrayList<>();
+}
