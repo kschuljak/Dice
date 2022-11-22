7cde8e41a580d1e7fc48a431aa57c665df4ba690
Katherine Schuljak <kschuljak@gmail.com>
2022-11-21 17:42:06 -0800

    databases tutorial

--- module-2/02_Ordering_Limiting_and_Grouping/tutorial/ordering-limiting-grouping-tutorial.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/tutorial/ordering-limiting-grouping-tutorial.sql
@@ -1,19 +1,46 @@
 -- Part one: ORDER BY
-
+SELECT topping_name
+FROM topping
+ORDER BY topping_name DESC;
 
 
 -- Part two: LIMIT
-
+SELECT sale_id
+	, total
+FROM sale
+ORDER BY total DESC
+LIMIT 5;
 
 
 -- Part three: Aggregate functions
+SELECT SUM(total) AS total_sales
+FROM sale
+WHERE customer_id = 37;
 
+SELECT MIN(total) AS min_sales
+	, MAX(total) AS max_sales
+	, ROUND(AVG(total), 2) AS avg_sales
+FROM sale;
+
+SELECT COUNT(*) AS times_used
+FROM pizza_topping
+WHERE topping_name = 'Pineapple';
 
 
 -- Part four: GROUP BY
-
+SELECT topping_name
+	, COUNT(*) AS times_used
+FROM pizza_topping
+GROUP BY topping_name
+ORDER BY times_used DESC;
 
 
 -- Part five: String concatenation
 
+SELECT first_name
+	|| ' '
+	|| last_name
+	AS full_name
+FROM customer
+ORDER BY last_name;
 
