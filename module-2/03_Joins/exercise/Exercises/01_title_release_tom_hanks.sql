3302a5e539d20e366fc0bd513a6f8279f3fca6e2
Katherine Schuljak <kschuljak@gmail.com>
2022-11-23 13:58:17 -0800

    databases joining exercise 1 added

--- module-2/03_Joins/exercise/Exercises/01_title_release_tom_hanks.sql
+++ module-2/03_Joins/exercise/Exercises/01_title_release_tom_hanks.sql
@@ -2,3 +2,13 @@
 -- Order the results by release date, newest to oldest.
 -- (47 rows)
 
+SELECT title
+	, release_date
+FROM movie AS m
+INNER JOIN movie_actor AS ma
+	ON ma.movie_id = m.movie_id
+INNER JOIN person AS p
+	ON p.person_id = ma.actor_id
+WHERE p.person_name = 'Tom Hanks'
+ORDER BY release_date;
+
