6bb7112b98374506b7440f50a1972fb742011d8c
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-11-30 04:20:57 -0700

    added exercise files

---
+++ module-2/07_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/model/Department.java
@@ -0,0 +1,35 @@
+package com.techelevator.projects.model;
+
+public class Department {
+
+	private int id;
+	private String name;
+
+	public Department() {}
+
+	public Department(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+}
