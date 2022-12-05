b9464c6e0e4514b07c8b193888ff27446f8499c5
Katherine Schuljak <kschuljak@gmail.com>
2022-12-05 14:16:48 -0800

    Consuming RESTful APIs pt 1 exercise finished

--- module-2/11_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -11,23 +11,31 @@ public class AuctionService {
 
 
     public Auction[] getAllAuctions() {
-        // call api here
-        return null;
+        
+        String url = API_BASE_URL;
+        Auction[] auctions = restTemplate.getForObject(url, Auction[].class);
+        return auctions;
     }
 
     public Auction getAuction(int id) {
-        // call api here
-        return null;
+
+        String url = API_BASE_URL+id;
+        Auction auction = restTemplate.getForObject(url, Auction.class);
+        return auction;
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
-        // call api here
-        return null;
+
+        String url = API_BASE_URL+"?title_like="+title;
+        Auction[] auctions = restTemplate.getForObject(url, Auction[].class);
+        return auctions;
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
-        // call api here
-        return null;
+
+        String url = API_BASE_URL+"?currentBid_lte="+price;
+        Auction[] auctions = restTemplate.getForObject(url, Auction[].class);
+        return auctions;
     }
 
 }
