cfd80863e5c3aa9306c124ebb21b381cf413384c
Katherine Schuljak <kschuljak@gmail.com>
2022-12-07 13:39:09 -0800

    server side api pt1 exercise added

--- module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,11 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/auctions")
@@ -10,8 +14,38 @@ public class AuctionController {
 
     private AuctionDao dao;
 
-    public AuctionController() {
+    public AuctionController()
+    {
         this.dao = new MemoryAuctionDao();
     }
+
+
+    @GetMapping(path="")
+    public List<Auction> list(@RequestParam(defaultValue = "") String title_like, @RequestParam(defaultValue = "0") double currentBid_lte)
+    {
+        List<Auction> auctions = dao.list();
+
+        if (title_like.length() > 0 && currentBid_lte == 0) auctions = dao.searchByTitle(title_like);
+        if (title_like.length() == 0 && currentBid_lte > 0) auctions = dao.searchByPrice(currentBid_lte);
+        if (title_like.length() > 0 && currentBid_lte > 0) auctions = dao.searchByTitleAndPrice(title_like, currentBid_lte);
+
+        return auctions;
+    }
+
+    @GetMapping(path="/{id}")
+    public Auction get(@PathVariable int id)
+    {
+        Auction auction = dao.get(id);
+        return auction;
+    }
+
+    @PostMapping(path="")
+    public Auction create(@RequestBody Auction auction)
+    {
+        Auction newAuction = dao.create(auction);
+        return newAuction;
+    }
+
+
 
 }
